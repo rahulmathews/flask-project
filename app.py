8ac8f3e3df70f3ccdb21d04280ac6121b71903bb
Rahul <27402666+rahulmathews@users.noreply.github.com>
2023-09-19 13:11:54 -0500

    Added serverless code

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, redirect
-
+from flask import Flask, jsonify, make_response, redirect
 from netflix import db, routes
+
 
 app = Flask(__name__)
 
 app.register_blueprint(routes.netflix_api, url_prefix='/netflix')
+
+
+@app.route("/")
+def hello_from_root():
+    return jsonify(message='Hello from root!')
+
+
+@app.route("/hello")
+def hello():
+    return jsonify(message='Hello from path!')
 
 
 @app.route("/")
@@ -15,3 +25,8 @@ def index():
 @app.route("/ping")
 def ping():
     return "pong"
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 404)
